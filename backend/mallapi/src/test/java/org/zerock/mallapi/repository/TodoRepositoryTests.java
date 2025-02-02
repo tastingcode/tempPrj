087e105a10c6fc3d6bd027b5351d69ed9de3cdc3
Soultreee <neverfidgh@naver.com>
2025-02-02 23:10:42 +0900

    테스트 코드 추가

---
+++ backend/mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -0,0 +1,77 @@
+package org.zerock.mallapi.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.domain.Todo;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@SpringBootTest
+@Log4j2
+public class TodoRepositoryTests {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    public void test1() {
+
+        log.info("--------------------");
+        log.info(todoRepository);
+
+    }
+
+    @Test
+    public void testInsert() {
+        for (int i = 1; i <= 100; i++) {
+            Todo todo = Todo.builder()
+                    .title("Title..." + i)
+                    .dueDate(LocalDate.of(2023, 12, 31))
+                    .writer("user00")
+                    .build();
+
+            todoRepository.save(todo);
+        }
+    }
+
+    @Test
+    public void testRead(){
+
+        //존재하는 번호로 확인
+        Long tno = 33L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+
+        Todo todo = result.orElseThrow();
+
+        log.info(todo);
+    }
+
+    @Test
+    public void testModify(){
+
+        Long tno = 33L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+
+        Todo todo = result.orElseThrow();
+        todo.changeTitle("Modified 33...");
+        todo.changeComplete(true);
+        todo.changeDueDate(LocalDate.of(2023, 10, 10));
+
+        todoRepository.save(todo);
+
+    }
+
+    @Test
+    public void testDelete(){
+        Long tno = 1L;
+
+        todoRepository.deleteById(tno);
+
+    }
+
+}
