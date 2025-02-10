9382f20c4193354635024247c1103e7d42ae5751
rh.kim <wiseitrees@gmail.com>
2025-02-10 20:23:21 +0900

    commit

--- backend/mallapi/src/main/java/org/zerock/mallapi/util/CustomFileUtil.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/util/CustomFileUtil.java
@@ -3,7 +3,12 @@ package org.zerock.mallapi.util;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -54,6 +59,17 @@ public class CustomFileUtil {
 
             try {
                 Files.copy(multipartFile.getInputStream(), savePath);
+
+                String contentType = multipartFile.getContentType();
+
+                if (contentType != null && contentType.startsWith("image")) {
+                    Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+
+                    Thumbnails.of(savePath.toFile())
+                            .size(200, 200)
+                            .toFile(thumbnailPath.toFile());
+                }
+
                 uploadNames.add(savedName);
             } catch (IOException e) {
                 throw new RuntimeException(e.getMessage());
@@ -62,4 +78,22 @@ public class CustomFileUtil {
         return uploadNames;
     }
 
+    public ResponseEntity<Resource> getFile(String fileName) {
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+        String separator = File.separator;
+        log.info("separator = " + separator);
+
+        if (!resource.isReadable()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.png");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try{
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
 }
